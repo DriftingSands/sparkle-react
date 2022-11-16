a0250d3b41407b2f5a733d27b6466fa32d13b122
Patrick Zimmermann <patrickmzimmermann@gmail.com>
2022-11-16 08:36:07 +0100

    fixed publishHost not being set as customHost after fetch. Fixed crossorigin property on dynamic preconnect

--- public/dataFetch.js
+++ public/dataFetch.js
@@ -1,21 +1,19 @@
-import fetchConfig from './initializeQueries.js';
-
+import fetchConfig from "./initializeQueries.js";
 
 function fallbackFetch(fetchConfig, variation, resolve) {
   fetch(`${fetchConfig.fallbackHost}/${fetchConfig.endpoint};variation=${variation}`)
-    .then((response) => {
-      return response.json()
+    .then(response => {
+      return response.json();
     })
-    .then((data) => {
+    .then(data => {
       window.customHost = fetchConfig.fallbackHost;
       resolve(data);
     })
-    .catch((error) => {
-      console.log('fallback host fetch failed')
-      resolve(null)
-    })
+    .catch(error => {
+      console.log("fallback host fetch failed");
+      resolve(null);
+    });
 }
-
 
 function attemptFetch(fetchConfig, variation, sparkleFetch = false, delay) {
   if (window[variation + "Data"]) {
@@ -23,11 +21,11 @@ function attemptFetch(fetchConfig, variation, sparkleFetch = false, delay) {
   }
   window[variation + "Data"] = new Promise(async (resolve, reject) => {
     if (delay) {
-      await new Promise(resolve => setTimeout(resolve, delay))
+      await new Promise(resolve => setTimeout(resolve, delay));
     }
     if (!fetchConfig.authorHost && !fetchConfig.publishHost) {
-      console.log('no author or publish host specified')
-      return fallbackFetch(fetchConfig, variation, resolve)
+      console.log("no author or publish host specified");
+      return fallbackFetch(fetchConfig, variation, resolve);
     }
     fetch(
       `${fetchConfig.authorHost}/${fetchConfig.endpoint}${
@@ -48,55 +46,57 @@ function attemptFetch(fetchConfig, variation, sparkleFetch = false, delay) {
         fetch(`${fetchConfig.publishHost}/${fetchConfig.endpoint};variation=${variation}`)
           .then(response => response.json())
           .then(data => {
-            window.customHost = fetchConfig.authorHost;
+            window.customHost = fetchConfig.publishHost;
             resolve(data);
           })
           .catch(error => {
             console.log("Publish url also failed.");
-            fallbackFetch(fetchConfig, variation, resolve)
+            fallbackFetch(fetchConfig, variation, resolve);
           });
       });
   });
 }
 
-
 if (window.innerWidth <= 820) {
   attemptFetch(fetchConfig, "mobile");
-  attemptFetch(fetchConfig, 'desktop', null, 5000)
+  attemptFetch(fetchConfig, "desktop", null, 5000);
 } else {
   attemptFetch(fetchConfig, "desktop");
-  attemptFetch(fetchConfig, 'mobile', null, 5000)
+  attemptFetch(fetchConfig, "mobile", null, 5000);
 }
 
-let preFetchUrl = fetchConfig.fallbackHost
+let preFetchUrl = fetchConfig.fallbackHost;
 
 if (fetchConfig.publishHost) {
-  preFetchUrl = fetchConfig.publishHost
+  preFetchUrl = fetchConfig.publishHost;
 }
 if (fetchConfig.authorHost) {
-  preFetchUrl = fetchConfig.authorHost
+  preFetchUrl = fetchConfig.authorHost;
 }
 
-const preconnectLink = document.createElement('link')
+const preconnectLink = document.createElement("link");
 
-preconnectLink.rel = 'preconnect'
-preconnectLink.crossorigin = ''
-preconnectLink.href = preFetchUrl
+preconnectLink.rel = "preconnect";
+preconnectLink.crossOrigin = "true";
+preconnectLink.href = preFetchUrl;
 
-document.head.appendChild(preconnectLink)
+document.head.appendChild(preconnectLink);
 
-const preconnectBiker = document.createElement('link')
+const preconnectBiker = document.createElement("link");
 
-preconnectBiker.rel = 'preload'
-preconnectBiker.fetchpriority = 'high'
-preconnectBiker.as = 'image'
-preconnectBiker.id = 'preload-biker'
-preconnectBiker.type = 'image/webp'
-preconnectBiker.href = `${preFetchUrl}/content/dam/sample-wknd-app/en/image-files/biker${ window.innerWidth <= 820 ? '_m' : ''}.png/_jcr_content/renditions/${window.innerWidth <= 820 ? 'mobile-vertical' : 'desktop'}.webp`
+preconnectBiker.rel = "preload";
+preconnectBiker.fetchpriority = "high";
+preconnectBiker.as = "image";
+preconnectBiker.id = "preload-biker";
+preconnectBiker.type = "image/webp";
+preconnectBiker.href = `${preFetchUrl}/content/dam/sample-wknd-app/en/image-files/biker${
+  window.innerWidth <= 820 ? "_m" : ""
+}.png/_jcr_content/renditions/${window.innerWidth <= 820 ? "mobile-vertical" : "desktop"}.webp`;
 
-document.head.appendChild(preconnectBiker)
+document.head.appendChild(preconnectBiker);
 
-{/* <link
+{
+  /* <link
   fetchpriority="high"
   rel="preload"
   href={`${preFetchUrl}/content/dam/sample-wknd-app/en/image-files/biker${ window.innerWidth <= 820 ? '_m' : ''}.png/_jcr_content/renditions/mobile-vertical.webp`}
@@ -104,4 +104,5 @@ document.head.appendChild(preconnectBiker)
   as="image"
   type="image/webp"
 />
-<link rel="preconnect" href={preFetchUrl} crossorigin /> */}
+<link rel="preconnect" href={preFetchUrl} crossorigin /> */
+}
